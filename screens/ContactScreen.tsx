f488b2b6382e27c8dbf69857e04e4abb62cd126c
Kevin Long <kevin.uu.long@gmail.com>
2026-01-27 22:22:39 -0500

    Start work with HCE
    This is very WIP and won't work for now unless you manually modify the Android config files.

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -1,38 +1,77 @@
 import Avatar from "@/components/Avatar";
 import PhoneNumbers from "@/components/PhoneNumbers";
 import { Button, StyleSheet, Text, View } from "react-native";
-import NfcManager, { Ndef, NfcTech } from 'react-native-nfc-manager';
-
+import { HCESession, NFCTagType4, NFCTagType4NDEFContentType } from "react-native-hce";
 
 import * as Contacts from "expo-contacts";
+import { File, Paths } from "expo-file-system";
 
-async function shareContact(contact: Contacts.Contact) {
-    console.log(contact);
+async function shareContact(contact: Contacts.ExistingContact) {
+    // console.log(contact);
 
-    let result = false;
+    // let result = false;
 
-    try {
-        await NfcManager.requestTechnology(NfcTech.Ndef);
+    // try {
+    //     await NfcManager.requestTechnology(NfcTech.Ndef);
 
-        const bytes = Ndef.encodeMessage([Ndef.textRecord("Hello NFC!")]);
+    //     const bytes = Ndef.encodeMessage([Ndef.textRecord("Hello NFC!")]);
 
-        if (bytes) {
-            await NfcManager.ndefHandler.writeNdefMessage(bytes);
-            result = true;
+    //     if (bytes) {
+    //         await NfcManager.ndefHandler.writeNdefMessage(bytes);
+    //         result = true;
+    //     }
+    // } catch (error) {
+    //     console.log(error);
+    // } finally {
+    //     NfcManager.cancelTechnologyRequest();
+    // }
+
+    // console.log(result);
+
+    // return result;
+
+    let session: HCESession | undefined;
+
+    console.log(session?.enabled);
+    if (!session?.enabled) {
+
+        const uri = await Contacts.writeContactToFileAsync({
+            id: contact.id,
+        });
+
+        console.log(uri);
+
+        if (uri) {
+            const file = new File(Paths.cache, `${contact.id}.vcf`);
+
+            // TODO: I know it won't work exactly as we want right now, but I want to get the actual vCard to transmit as 
+            // text, the new Expo FileSystem API is just giving me a little trouble right now
+
+
+            console.log("Starting share!")
+            const tag = new NFCTagType4({
+                type: NFCTagType4NDEFContentType.Text,
+                content: JSON.stringify(contact),
+                writable: false,
+            });
+
+
+
+            session = await HCESession.getInstance();
+            session.setApplication(tag);
+            await session.setEnabled(true);
+            console.log("Share ready!");
         }
-    } catch (error) {
-        console.log(error);
-    } finally {
-        NfcManager.cancelTechnologyRequest();
+
+    } else {
+        console.log("Stopping!")
+        await session.setEnabled(false);
     }
 
-    console.log(result);
-
-    return result;
 }
 
 
-export default function ContactScreen({ contact }: { contact: Contacts.Contact }) {
+export default function ContactScreen({ contact }: { contact: Contacts.ExistingContact }) {
     return (
         <>
             <View style={styles.head}>
