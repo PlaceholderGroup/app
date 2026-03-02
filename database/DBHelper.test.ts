d7eec7472b5377514ab848e8ed02df39760a99db
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2026-03-02 13:55:11 -0500

    last before merge attempt
    Overall, db code should be working as expected, need to look into testing for it soon. Expo-contacts are expected to be implemented soon.

--- database/DBHelper.test.ts
+++ database/DBHelper.test.ts
@@ -1,29 +1,17 @@
 
-import DBHelper from './DBHelper';
+// import DBHelper from './DBHelper';
 
-describe('DBHelper Tests', () => {
-    //init db once for all tests
-    beforeAll(async () => {
-        await DBHelper.initDB();
-    });
+// describe('DBHelper Tests', () => {
+//     //init db once for all tests
 
-    afterAll(async () => {
-        await DBHelper.closeDB();
-    })
-    
-    test('Should init the db', async () => {
-        await DBHelper.initDB();
-        expect(DBHelper).toBeDefined();
-    });
+//     test('should create social obj', async () => {
+//         const result = await DBHelper.createSocialObj('tester2');
+//         expect(result).toBeGreaterThan(0);
+//     });
 
-    test('should create user', async () => {
-        const result = await DBHelper.createUser('tester2');
-        expect(result).toBeGreaterThan(0);
-    });
+//     test('should create contact', async () => {
+//         const result = await DBHelper.createContactObj(1,'tester1','personal');
+//         expect(result).toBeGreaterThan(0);
+//     });
 
-    test('should create contact', async () => {
-        const result = await DBHelper.createContact(1,'tester1','personal');
-        expect(result).toBeGreaterThan(0);
-    });
-
-});
+// });
--- database/NativeContact.ts
+++
@@ -1,3 +0,0 @@
-
-// How we want to do this:
-// create functions to call for permissions/request and receive contact values?s
