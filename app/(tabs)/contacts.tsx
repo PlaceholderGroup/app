d9ac024ce0e25f73a5a0c935ef37c1c41b3e1884
Kevin Long <kevin.uu.long@gmail.com>
2026-04-04 23:52:48 -0400

    Start styling the contacts list
    I realized that although this screen got designed. no one ever implemented it.
    
    This is a start.

--- app/(tabs)/contacts.tsx
+++ app/(tabs)/contacts.tsx
@@ -1,16 +1,65 @@
-import { FlatList } from "react-native";
+import { SectionList, StyleSheet, Text, View } from "react-native";
 
 import ContactsListitem from "@/components/ContactsListItem";
+import SearchBar from "@/components/SearchBar";
 import TabsSafeAreaView from "@/components/TabsSafeAreaView";
 import { ContactsContext } from "@/contexts/ContactsContext";
 import * as Contacts from "expo-contacts";
-import { useCallback, useContext } from "react";
+import { useCallback, useContext, useMemo, useState } from "react";
 
 export default function ContactsScreen() {
 
     const { contacts } = useContext(ContactsContext);
-    const renderItem = useCallback(({item}: {item: Contacts.ExistingContact}) => {
-        return <ContactsListitem id={item.id} name={item.name} photo={item.image?.uri} />
+    const [searchQuery, setSearchQuery] = useState<string>("");
+
+    const sections = useMemo(() => {
+        if (contacts) {
+            // TODO: I can probably improve this search
+            const filtered = contacts.filter((contact) => contact.name?.toLocaleLowerCase()?.includes(searchQuery.toLowerCase()));
+
+            // Group by first letter
+            const groups = filtered.reduce((previous, contact) => {
+                const section = contact.name?.at(0)?.toUpperCase() || "";
+                const key = /[A-Z]/.test(section) ? section : "#";
+
+                if (!previous[key]) previous[key] = [];
+                previous[key].push(contact);
+                return previous;
+            }, {} as Record<string, Contacts.ExistingContact[]>);
+
+            // Sort the groups
+            return Object.keys(groups)
+                .sort((a, b) => {
+                    if (a === "#") return 1;
+                    if (b === "#") return -1;
+                    return a.localeCompare(b);
+                })
+                .map((title) => ({
+                    title,
+                    data: groups[title].sort(),
+                }));
+        }
+        return [];
+    }, [contacts, searchQuery])
+
+    const renderItem = useCallback(({ item, index, section }: { item: Contacts.ExistingContact, index: number, section: any }) => {
+        return (
+            <View style={[
+                styles.contact,
+                (index === 0) && styles.firstContact,
+                (index === section.data.length - 1) && styles.lastContact,
+            ]}>
+                <ContactsListitem id={item.id} name={item.name} photo={item.image?.uri} />
+            </View>
+        )
+    }, []);
+
+    const renderSectionHeader = useCallback(({ section: { title } }: { section: { title: string } }) => {
+        return (
+            <View style={styles.header}>
+                <Text style={styles.headerLabel}>{title}</Text>
+            </View>
+        )
     }, []);
 
     // NOTE: Probably unnecessary, but does probably provide a very slight performance increase
@@ -20,15 +69,60 @@ export default function ContactsScreen() {
     // https://reactnative.dev/docs/sectionlist
     return (
         <TabsSafeAreaView>
+            <View style={styles.searchContainer}>
+                {/* TODO: Can I put this in the Navbar? Or at least make it look like it is? */}
+                <SearchBar placeholder="Search contacts" value={searchQuery} onChangeText={setSearchQuery} />
+            </View>
+
             {contacts !== undefined &&
-                <FlatList
-                    data={contacts}
+                <SectionList
+                    sections={sections}
                     renderItem={renderItem}
+                    renderSectionHeader={renderSectionHeader}
                     keyExtractor={keyExtractor}
                     removeClippedSubviews={true}
                     windowSize={16}
+                    contentContainerStyle={styles.list}
                 />
             }
         </TabsSafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    list: {
+        display: "flex",
+        flexDirection: "column",
+        padding: 20,
+        paddingTop: 0,
+        alignSelf: "stretch",
+    },
+    header: {
+        marginVertical: 10,
+    },
+    headerLabel: {
+        fontFamily: "Lexend_400Regular",
+    },
+    contact: {
+        display: "flex",
+        backgroundColor: "#FFFFFF",
+        borderColor: "#CCCCCC",
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+    },
+    firstContact: {
+        borderTopWidth: 1,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        paddingTop: 10,
+    },
+    lastContact: {
+        borderBottomWidth: 1,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        paddingBottom: 10,
+    },
+    searchContainer: {
+        padding: 20,
+    }
+})
---
+++ components/SearchBar.tsx
@@ -0,0 +1,46 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { StyleSheet, TextInput, TextInputProps, View } from "react-native";
+
+
+
+export default function SearchBar({ ...rest }: TextInputProps) {
+    return (
+        <View style={[
+            styles.container,
+        ]}>
+            <MaterialIcons name="search" size={24} style={styles.icon} />
+            <TextInput
+                clearButtonMode="while-editing"
+                style={styles.textBox}
+                {...rest}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        paddingLeft: 20,
+        backgroundColor: "rgba(255, 255, 255, 0.10)",
+        borderColor: "#CCCCCC",
+        borderWidth: 1,
+        borderRadius: 30,
+        alignItems: "center",
+        gap: 10,
+    },
+    textBox: {
+        flex: 1,
+        height: 60,
+        margin: 0,
+        padding: 0,
+        color: "#626262",
+        fontFamily: "Lexend_400Regular",
+        fontWeight: 400,
+        fontSize: 16,
+    },
+    icon: {
+        color: "#626262",
+    }
+});
